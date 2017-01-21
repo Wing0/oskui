fffbef8f89bd12ac0a91aafed65d5cf4cad08bdf
Oskari Vinko <vinkoo@student.ethz.ch>
2017-01-21 12:59:30 +0100

    Make it installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='oskui',
+      version='0.1',
+      description='Set of command line user interface tools',
+      url='https://github.com/Wing0/oskui.git',
+      author='Oskari Vinko',
+      author_email='oskari.vinko@gmail.com',
+      license='MIT',
+      packages=['oskui'],
+      zip_safe=False)
