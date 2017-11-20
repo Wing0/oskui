e80904da5cba570aaf12cc8fe0e5fc3e454ce63f
Oskari Vinko <vinkoo@student.ethz.ch>
2017-11-20 21:10:39 +0100

    Actually fixed the download link and version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='oskui',
-      version='0.13.4',
+      version='0.13.5',
       description='Set of command line user interface tools',
       url='https://github.com/Wing0/oskui.git',
       author='Oskari Vinko',
@@ -9,6 +9,6 @@ setup(name='oskui',
       license='MIT',
       packages=['oskui'],
       url="https://github.com/Wing0/oskui",
-      download_utl="https://github.com/Wing0/oskui/archive/0.13.4.tar.gz",
+      download_utl="https://github.com/Wing0/oskui/archive/0.13.5.tar.gz",
       keywords=['user', 'interface', 'ui', 'cmd'],
       zip_safe=False)
