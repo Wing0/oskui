6bdc1b02ea10a967dc30e19ebc1ea923f8fbc34f
Oskari Vinko <vinkoo@student.ethz.ch>
2017-11-20 21:53:40 +0100

    Small fixes to pypi installation

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from distutils.core import setup
 
-setup(name='oskui',
-      version='0.13.5',
-      description='Set of command line user interface tools',
-      url='https://github.com/Wing0/oskui.git',
-      author='Oskari Vinko',
-      author_email='oskari.vinko@gmail.com',
-      license='MIT',
-      packages=['oskui'],
-      url="https://github.com/Wing0/oskui",
-      download_utl="https://github.com/Wing0/oskui/archive/0.13.5.tar.gz",
-      keywords=['user', 'interface', 'ui', 'cmd'],
-      zip_safe=False)
+setup(
+    name='oskui',
+    packages=['oskui'],
+    version='0.13.6',
+    description='Set of command line user interface tools',
+    author='Oskari Vinko',
+    author_email='oskari.vinko@gmail.com',
+    license='MIT',
+    url="https://github.com/Wing0/oskui",
+    download_url="https://github.com/Wing0/oskui/archive/0.13.6.tar.gz",
+    keywords=['user', 'interface', 'ui', 'cmd'],
+    zip_safe=False)
